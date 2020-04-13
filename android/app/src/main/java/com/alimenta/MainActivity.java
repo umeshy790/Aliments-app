f0805f9b25bda3cfae17f62b51bb87544f17cf60
Umesh <umeshy790@gmail.com>
2020-04-13 09:18:42 +0530

    refactoring

--- android/app/src/main/java/com/alimenta/MainActivity.java
+++ android/app/src/main/java/com/alimenta/MainActivity.java
@@ -1,10 +1,10 @@
 package com.alimenta;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +12,5 @@ public class MainActivity extends ReactActivity {
     super.onCreate(savedInstanceState);
   }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "Alimenta";
-  }
+  
 }
